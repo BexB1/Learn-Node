a65901f4e9f511162aac545162705fc3d3a5be67
Bex B <bexb1@hotmail.com>
2017-12-16 18:01:02 +0000

    First pug template built.

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const bex = { name: "Bex", age: "Definitely not 30.", cool: true };
+  // res.send(bex);
+  // res.json(req.query);
+  res.render('hello', )
 });
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
+})
 
 module.exports = router;
